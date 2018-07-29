43cee22fc21ef36d5544313d9561d60c35d3066c
Enmanuel <enmachs@gmail.com>
2018-07-29 18:05:51 -0400

    First commit

---
+++ app/entries/app.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+// Import app styles
+// require('./scss/style.scss');
+
+// Import component
+import MyComponent from './../card/card-layout.jsx';
+
+ReactDOM.render(<MyComponent />, document.getElementById('app'));
---
+++ webpack.dev.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+
+module.exports = {
+  // JavaScript entry point
+  entry: {
+    'app': path.resolve(__dirname, 'app/entries/app.js'),
+  },
+  // JavaScrip bundle file
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].js'
+  },
+  // Setup server
+  devServer: {
+    inline: true,
+    port: 9000
+  },
+  devtool: 'eval-source-map',
+  module: {
+    // JS, JSX and SASS loaders
+    loaders: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react', 'stage-2']
+        }
+      },
+      {
+        test: /\.(scss|sass)$/,
+        loaders: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.(jpg|png|gif|svg)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 1000000,
+            fallback: 'file-loader',
+            name: 'images/[name].[hash].[ext]'
+          }
+        }
+      }
+    ]
+  }
+};
